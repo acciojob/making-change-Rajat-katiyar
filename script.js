11a3ffc7b46e52aee0b53473249c5f247c2f33e4
Rajat Katiyar <123207263+Rajat-katiyar@users.noreply.github.com>
2024-12-09 13:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const makeChange = (c) => {
   // your name here
+	const change = {};  
+  
+  // Calculate the number of quarters  
+  change.q = Math.floor(c / 25);  
+  c = c % 25; // Remainder after quarters  
+  
+  // Calculate the number of dimes  
+  change.d = Math.floor(c / 10);  
+  c = c % 10; // Remainder after dimes  
+  
+  // Calculate the number of nickels  
+  change.n = Math.floor(c / 5);  
+  c = c % 5; // Remainder after nickels  
+  
+  // What remains is the number of pennies  
+  change.p = c; // This is the remaining amount in cents, which are pennies  
+  
+  return change;  
 };
 
 // Do not the change the code below
